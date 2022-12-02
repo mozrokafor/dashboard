0a2344c6383a99aa0512d7dde04d89d25c996290
mozrokafor <94625718+mozrokafor@users.noreply.github.com>
2022-12-02 11:51:09 -0500

    checking core pkg

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+const github = require('@actions/github')
+const core = require('@actions/core')
+
 async function getData() {
-    console.log('Get Data from Github API here');
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
+    console.log('Get Data from Github API here token', GITHUB_TOKEN)
+
+    const octokit = github.getOctokit(GITHUB_TOKEN)    
+
+    const res = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows', {
+        owner: 'mozilla-mobile',
+        repo: 'mozilla-vpn-client'
+    })
+
+    console.log(res);
 }
 
 getData()
