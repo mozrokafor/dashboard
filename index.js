0a61c45d5465e2771f9bd04f4dab608fce5320df
mozrokafor <94625718+mozrokafor@users.noreply.github.com>
2022-12-02 12:01:19 -0500

    wip 3

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const github = require('@actions/github')
 const core = require('@actions/core')
 
-async function getData() {
-    const octokit = github.getOctokit(process.env.GITHUB_TOKEN)    
+const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
 
+async function getData() {
+    const res = await getAllWorkflows();
+    console.log(res.workflows[0])
+}
+
+getData()
+
+const getAllWorkflows = async () => {
     const res = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows', {
         owner: 'mozilla-mobile',
         repo: 'mozilla-vpn-client'
     })
 
-    console.log(res);
+    return res.data
 }
-
-getData()
